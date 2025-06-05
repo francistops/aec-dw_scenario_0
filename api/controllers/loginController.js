6a616f5fd64ab398d6c666a70d79f3f0afc503e2
francistops <65456051+francistops@users.noreply.github.com>
2025-06-05 10:41:56 -0400

    fix one oppsy!

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -7,8 +7,3 @@ const crypto = require("crypto");
 // la on le hash mais est le bonne endroit?
 // const hashBuffer = await crypto.subtle.digest('SHA-256', data);
 // const userPassHash = crypto.createHash("sha256").update(userSentPassword).digest("hex");
-
-
-exports.login = async (req, res) => {
-  console.log("---in controller debugLogin---");
-  
